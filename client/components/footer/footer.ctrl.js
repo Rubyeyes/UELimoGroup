14b99487bed43c8dab4fb94786164ee815061792
Rubyeyes <catian315@gmail.com>
2016-08-07 13:18:22 -0700

    footer bottom

--- client/components/footer/footer.ctrl.js
+++ client/components/footer/footer.ctrl.js
@@ -8,15 +8,17 @@ angular.module('MyApp')
 
 		$scope.$on('$stateChangeSuccess',function(){
 	        var path = $location.path();
-			if(path == '/home') {
-				$scope.colorFooter = {color: 'white'};
+			if(path === '/home' || path === '/login' || path ==='/register') {
+				if (path == '/home') {
+					$scope.colorFooter = {color: 'white'};
+				} else {
+					$scope.colorFooter = {color: 'black'};
+				}
 				$scope.footPosition = {			
 				    position: 'fixed',
 				    bottom: 0,
 				    width: '100%'
 				}		
-			} else if(path == '/login' || '/register') {				
-				$scope.colorFooter = {color: 'black'};	
 			} else {
 				$scope.colorFooter = {color: 'black'};
 				$scope.footPosition = {			
